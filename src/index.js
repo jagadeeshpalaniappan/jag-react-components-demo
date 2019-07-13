8e039b32631410795bd91be79568cfcd4901c97c
Jagadeesh Palaniappan <jagadeeshthegeek@gmail.com>
2019-07-12 18:03:51 -0700

    JagTestComponent1,2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Example, { SideNav } from "jag-react-components-test";
+import JagTestComponentDefault, {
+  JagTestComponent1,
+  JagTestComponent2
+} from "jag-react-components";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h3 className="title">Components Demo: </h3>
       <hr />
-      <Example />
+      <h3>Component 1: </h3>
+      <JagTestComponentDefault />
       <hr />
-      <SideNav />
+      <h3>Component 2: </h3>
+      <JagTestComponent1 />
+      <hr />
+      <h3>Component 2: </h3>
+      <JagTestComponent2 color="deepskyblue" />
     </div>
   );
 }
